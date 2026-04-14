cc1efa6868a241bc7e20ac87eab7f5c0166e710d
student <student@localhost.localdomain>
2026-04-14 18:53:38 +0300

    Успешный CI: корректный код

---
+++ calc.py
@@ -0,0 +1,7 @@
+def average(numbers):
+	return sum(numbers) / len(numbers)
+
+if __name__ == "__main__":
+	test_data = [10, 20, 30, 40]
+	print(f"Среднее: {average(test_date)}")
+
---
+++ test_calc.py
@@ -0,0 +1,8 @@
+python
+
+import calc
+
+def test_average():
+	assert calc.average([1, 2, 3]) == 2.0
+	assert calc.average([10, 20]) == 15.0
+
